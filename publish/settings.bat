865d63e946cc04e1841769c6bc90f5569d41df1e
gadjgasan1997 <gadj.gasan1997@gmail.com>
2023-08-06 02:06:09 +0300

    Добавил метод расширения, позволяющий убирать все пробелы в строке

--- publish/settings.bat
+++ publish/settings.bat
@@ -1,4 +1,4 @@
 set projectName=Essentials.Func.Utils.Core
 set packageName=Essentials.Func.Utils
-set packageVersion=1.0.0.3
+set packageVersion=1.0.0.4
 set configuration=Release
---
+++ src/Essentials.Func.Utils.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Essentials.Func.Utils.Extensions;
+
+/// <summary>
+/// Методы расширения для строк
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Убирает все пробелы в строке
+    /// </summary>
+    /// <param name="string">Исходная строка</param>
+    /// <returns>Строка без пробелов</returns>
+    public static string FullTrim(this string @string) => @string.Replace(" ", string.Empty);
+}

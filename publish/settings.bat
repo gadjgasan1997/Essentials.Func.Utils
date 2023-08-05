ef226cc67eff008a8b7cc1348fcd74292ca95004
gadjgasan1997 <gadj.gasan1997@gmail.com>
2023-08-06 01:39:54 +0300

    Добавил хелперы для работы с Json

--- publish/settings.bat
+++ publish/settings.bat
@@ -1,4 +1,4 @@
 set projectName=Essentials.Func.Utils.Core
 set packageName=Essentials.Func.Utils
-set packageVersion=1.0.0.1
+set packageVersion=1.0.0.3
 set configuration=Release
---
+++ src/Essentials.Func.Utils.Core/Helpers/JsonHelpers.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Essentials.Func.Utils.Helpers;
+
+/// <summary>
+/// Хелперы для работы с Json
+/// </summary>
+public static class JsonHelpers
+{
+    private static readonly JsonSerializerSettings _settings = new()
+    {
+        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+    };
+
+    /// <summary>
+    /// Серилизует объект в строку
+    /// </summary>
+    /// <param name="obj">Объект</param>
+    /// <typeparam name="T">Тип объекта</typeparam>
+    /// <returns>Строка</returns>
+    public static string Serialize<T>(T? obj) => JsonConvert.SerializeObject(obj, _settings);
+}
